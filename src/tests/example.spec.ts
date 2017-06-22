87f28643c01af1b1ba8383dc3d33f89a6e3abc41
fscfelipe <felipesobreira.c@hotmail.com>
2017-06-22 17:35:48 -0300

    leve update

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -12,7 +12,3 @@ describe('Testes da classe Utils', function() {
 
 
 });
-
-//Que o teste acima da erro, olhe o Prompt de comando para ver o erro.
-//P.S.: Os erros são executados em realtime, então se você corrigir acima
-//a linha de comando já apresentará o resultado.
